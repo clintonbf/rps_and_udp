7c3ee742f9f4a63b303a057b5f489266e3829252
Clint <55072157+clintonbf@users.noreply.github.com>
2021-04-14 15:33:54 -0700

    minor: added waiting for data code to setUid and getPlayInvitation

--- app/src/main/java/ca/bcit/rps_and_udp/RPS.java
+++ app/src/main/java/ca/bcit/rps_and_udp/RPS.java
@@ -146,196 +146,6 @@ public class RPS extends AppCompatActivity {
                 });
 
                 thread.start();
-
-
-//                Thread thread = new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                         try {
-//                            /*
-//                             connectToServer()
-//                             */
-//                            InetAddress addr = InetAddress.getByName(Environment.HOST);
-//                            socket = new Socket(Environment.HOST, Environment.PORT);
-//                            toServer = new PrintStream(socket.getOutputStream());
-//                            fromServer = socket.getInputStream();
-//                            /*
-//                             /connectToServer()
-//                             */
-////                             new connectToServerClass().execute();
-//
-//                             /*
-//                             sendHandshake()
-//                              */
-//                            final int uid              = 0;
-//                            final int CONFIRMATION     = 1;
-//                            final int CONFIRM_RULESET  = 1;
-//                            final int payload_length   = 2;
-//                            final int PROTOCOL_VERSION = 1;
-//                            final int GAME_ID          = 2;
-//
-//                            final int[] handshakePayload = { PROTOCOL_VERSION, GAME_ID };
-//
-//                            RequestPacket req = new RequestPacket(uid, CONFIRMATION,
-//                                    CONFIRM_RULESET, payload_length, handshakePayload);
-//                            final byte[] handshake = req.toBytes();
-//
-//                            toServer.write(handshake);
-//                            /*
-//                            / sendHandshake()
-//                             */
-//
-//                            while (true) {
-//                                try {
-//                                    if (fromServer.available() > 0) {
-//                                        /*
-//                                        setUid()
-//                                         */
-//                                        byte[] packet = new byte[7];
-//
-//                                        int bytesRead = fromServer.read(packet);
-//
-//                                        ByteBuffer bb = ByteBuffer.wrap(packet);
-//
-//                                        if (bytesRead < 0) {
-//                                            Log.e(STREAM, "No data received in handshake");
-//                                            socket.close();
-//                                            System.exit(1);
-//                                        }
-//
-//                                        int[] welcomePayload = new int[bb.get(2)];
-//
-//                                        welcomePayload[0] = bb.getInt(3);
-//
-//                                        Packet welcomePacket = new Packet((int)bb.get(0), (int)bb.get(1), (int)bb.get(2), welcomePayload);
-//
-//                                        player = new GameData(welcomePacket.getPayload()[0]);
-//                                        Log.d(STREAM, welcomePacket.toString());
-//                                        Log.d(STREAM, player.toString());
-//                                        /*
-//                                        / setUid()
-//                                         */
-//
-//                                        /*
-//                                        getPlayInvitation()
-//                                         */
-//
-//                                        bytesRead = fromServer.read(packet);
-//                                        if (bytesRead < 0) {
-//                                            Log.e(STREAM, "No data received in handshake");
-//                                            socket.close();
-//                                            System.exit(1);
-//                                        }
-//
-//                                        int[] updatePayload = new int[packet[2]];
-//                                        int index = 0;
-//
-//                                        for (int i = 3; i < 3 + packet[2]; i++) {
-//                                            updatePayload[index] = packet[i];
-//                                            index++;
-//                                        }
-//
-//                                        Packet invitePacket = new Packet(packet[0], packet[1], packet[2], updatePayload);
-//                                        Log.d(INVITE, invitePacket.toString());
-//                                        /*
-//                                        / getPlayInvitation()
-//                                         */
-//
-//                                        //Send your play
-//
-//                                        /*
-//                                        janky-ass nonsense
-//                                         */
-//
-//                                        while (playChoice == 10000);
-//
-//                                        final int GAME_ACTION      = 4;
-//                                        final int MOVE_MADE        = 2;
-//                                        final int play_payload_length   = 1;
-//
-//                                        final int[] payload = { playChoice };
-//
-//                                        RequestPacket playReq = new RequestPacket(player.getUid(),
-//                                                GAME_ACTION, MOVE_MADE, play_payload_length,
-//                                                payload);
-//                                        Log.d(PLAY, playReq.toString());
-//                                        final byte[] playPacket = req.toBytes();
-//                                        toServer.write(playPacket);
-//
-//                                        byte[] playAckPacket = new byte[3];
-//                                        bytesRead = fromServer.read(playAckPacket);
-//                                        if (bytesRead < 0) {
-//                                            Log.e(STREAM, "No data received in play acknowledgement");
-//                                            socket.close();
-//                                            System.exit(1);
-//                                        }
-//
-//                                        if (playAckPacket[0] == 10) {
-//                                            Log.d(PLAY_ACK, "Received acknowledgement from server");
-//                                        } else {
-//                                            Log.e(PLAY_ACK, "Server sent strange msg." + playAckPacket[0]);
-//                                        }
-//
-//                                        /*
-//                                        / janky-ass nonsense
-//                                         */
-//
-//                                        byte[] outcomePacketFromServer = new byte[5];
-//                                        bytesRead = fromServer.read(outcomePacketFromServer);
-//                                        if (bytesRead < 0) {
-//                                            Log.e(STREAM, "No data received in game outcome");
-//                                            socket.close();
-//                                            System.exit(1);
-//                                        }
-//
-//                                        int[] outcomePayload = new int[outcomePacketFromServer[2]];
-//                                        index = 0;
-//                                        for (int i = 3; i < 3 + outcomePacketFromServer[2]; i++) {
-//                                            outcomePayload[index] = outcomePacketFromServer[i];
-//                                            index++;
-//                                        }
-//
-//                                        Log.d(STREAM, "Received game outcome " + outcomePayload[0]);
-//
-//                                        Packet outcomePacket = new Packet(packet[0], packet[1], packet[2], outcomePayload);
-//                                        opponentsMove = outcomePayload[1];
-//
-//                                        switch (outcomePacket.payload[0]) {
-//                                            case 1:
-//                                                Log.d(OUTCOME, "Win. Opponent played " + outcomePayload[1]);
-//                                                gameOutcome = Outcomes.WIN.ordinal();
-//                                                break;
-//                                            case 2:
-//                                                Log.d(OUTCOME, "Loss. Opponent played " + outcomePayload[1]);
-//                                                gameOutcome = Outcomes.LOSS.ordinal();
-//
-//                                                break;
-//                                            case 3:
-//                                                Log.d(OUTCOME, "Tie. Opponent played " + outcomePayload[1]);
-//                                                gameOutcome = Outcomes.TIE.ordinal();
-//
-//                                                break;
-//                                            default:
-//                                                Log.d(OUTCOME, "ERROR");
-//                                                break;
-//                                        }
-//
-//                                        Log.d(OUTCOME, "Bye");
-//                                        socket.close();
-//                                    }
-//                                } catch (IOException e) {
-//                                    e.printStackTrace();
-//                                }
-//                            }
-//                        } catch (SocketException se) {
-//                            Log.e("CONNECTION:", "Unable to connect");
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                });
-//
-//                thread.start();
             }
         });
     }
@@ -391,9 +201,20 @@ public class RPS extends AppCompatActivity {
     }
 
     private void setUid() {
-        //Read a message
         byte[] packet = new byte[7];
-        
+
+        boolean waitingForData = true;
+
+        while (waitingForData) {
+            try {
+                if (fromServer.available() > 0) {
+                    waitingForData = false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         try {
            int bytesRead = fromServer.read(packet);
 
@@ -419,6 +240,18 @@ public class RPS extends AppCompatActivity {
     private void getPlayInvitation() {
          final int UPDATE = 20;
          byte[] packet = new byte[3];
+
+        boolean waitingForData = true;
+
+        while (waitingForData) {
+            try {
+                if (fromServer.available() > 0) {
+                    waitingForData = false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
          try {
              int bytesRead = fromServer.read(packet);
